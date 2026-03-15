7d87d8271f95945a1b8bec44b2fc35c67d594840
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-16 03:33:14 +0400

    Add files via upload

--- public/wheel.js
+++ public/wheel.js
@@ -2,6 +2,9 @@
 const canvas=document.getElementById("wheel");
 const ctx=canvas.getContext("2d");
 
+const spinSound=document.getElementById("spinSound");
+const winSound=document.getElementById("winSound");
+
 const segments=44;
 const values=[
 ...Array(37).fill("0 AZN"),
@@ -49,7 +52,11 @@ ctx.restore();
 
 draw();
 
-function spin(){
+function spin(user="User"){
+
+document.getElementById("spinInfo").innerText="🎡 Çarx "+user+" üçün fırlanır";
+
+spinSound.play();
 
 let duration=15000;
 let start=Date.now();
@@ -64,11 +71,58 @@ angle+=0.35*(1-t);
 draw();
 requestAnimationFrame(frame);
 
+}else{
+
+spinSound.pause();
+finish();
+
 }
 
 }
 
 frame();
+
+}
+
+function finish(){
+
+let result=values[Math.floor(Math.random()*values.length)];
+
+if(result==="0 AZN"){
+
+document.getElementById("result").innerText="😢 Uduzdunuz";
+
+}else{
+
+winSound.play();
+document.getElementById("result").innerText="🎉 Qazandınız: "+result;
+
+}
+
+countdown();
+
+}
+
+function countdown(){
+
+let t=10;
+
+let el=document.getElementById("countdown");
+
+let i=setInterval(()=>{
+
+el.innerText="Növbəti spin "+t;
+
+t--;
+
+if(t<0){
+
+clearInterval(i);
+el.innerText="";
+
+}
+
+},1000);
 
 }
 
