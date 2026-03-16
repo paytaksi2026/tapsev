4090d872868a945091d3e7b76db95d8d0ec380fe
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-16 04:11:51 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,136 +1,57 @@
 
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const { WebcastPushConnection } = require("tiktok-live-connector");
+const express=require("express");
+const http=require("http");
+const {Server}=require("socket.io");
+const {WebcastPushConnection}=require("tiktok-live-connector");
 
-const TIKTOK_USERNAME = "xeberx.az";
+const TIKTOK_USERNAME="xeberx.az";
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const app=express();
+const server=http.createServer(app);
+const io=new Server(server);
 
 app.use(express.static("public"));
-app.use("/admin", express.static("admin"));
 
-let queue = [];
-let spinning = false;
-
-let likeCounter = {};
-let giftCounter = {};
-
-let likeTotals = {};
-let giftTotals = {};
+let likeTotals={};
+let giftTotals={};
+let winners=[];
 
 function topList(obj){
-  return Object.entries(obj)
-    .sort((a,b)=>b[1]-a[1])
-    .slice(0,10);
+return Object.entries(obj).sort((a,b)=>b[1]-a[1]).slice(0,10);
 }
 
 function broadcastTop(){
-  io.emit("topLike", topList(likeTotals));
-  io.emit("topGift", topList(giftTotals));
+io.emit("topLike",topList(likeTotals));
+io.emit("topGift",topList(giftTotals));
+io.emit("topWinners",winners.slice(0,10));
 }
 
-function getRandomSegment(){
-  const segments=[
-    ...Array(37).fill(0),
-    ...Array(4).fill(1),
-    ...Array(2).fill(2),
-    3
-  ];
-  return segments[Math.floor(Math.random()*segments.length)];
-}
+const tiktok=new WebcastPushConnection(TIKTOK_USERNAME);
 
-function processQueue(){
+tiktok.connect().then(()=>{
+console.log("TikTok connected");
+});
 
-  if(spinning) return;
-  if(queue.length === 0) return;
+tiktok.on("like",data=>{
 
-  spinning = true;
+const user=data.uniqueId;
 
-  const user = queue.shift();
-  io.emit("queueUpdate", queue);
+likeTotals[user]=(likeTotals[user]||0)+data.likeCount;
 
-  const result = getRandomSegment();
-
-  io.emit("spinStart",{user,result});
-
-  setTimeout(()=>{
-
-    io.emit("spinResult",{user,result});
-
-    spinning = false;
-
-    setTimeout(processQueue,10000);
-
-  },15000);
-
-}
-
-const tiktokLiveConnection = new WebcastPushConnection(TIKTOK_USERNAME);
-
-tiktokLiveConnection.connect()
-.then(state => {
-
-console.log("Connected to TikTok LIVE:", state.roomId);
-io.emit("tiktokStatus","connected");
-
-})
-.catch(err => {
-
-console.error("TikTok connection failed", err);
-io.emit("tiktokStatus","failed");
+broadcastTop();
 
 });
 
-tiktokLiveConnection.on("like", data => {
+tiktok.on("gift",data=>{
 
-const user = data.uniqueId;
+const user=data.uniqueId;
 
-likeTotals[user] = (likeTotals[user]||0) + data.likeCount;
+giftTotals[user]=(giftTotals[user]||0)+1;
+
 broadcastTop();
-
-likeCounter[user] = (likeCounter[user]||0) + data.likeCount;
-
-if(likeCounter[user] >= 100){
-
-likeCounter[user] = 0;
-
-queue.push(user);
-
-io.emit("queueUpdate", queue);
-
-processQueue();
-
-}
-
-});
-
-tiktokLiveConnection.on("gift", data => {
-
-const user = data.uniqueId;
-
-giftTotals[user] = (giftTotals[user]||0) + 1;
-broadcastTop();
-
-giftCounter[user] = (giftCounter[user]||0) + 1;
-
-if(giftCounter[user] >= 100){
-
-giftCounter[user] = 0;
-
-queue.push(user);
-
-io.emit("queueUpdate", queue);
-
-processQueue();
-
-}
 
 });
 
 server.listen(3000,()=>{
- console.log("Server running with TikTok connector + TOP panels");
+console.log("Modern wheel server running");
 });
