eaceea47dee4e2a3c8d36df0d21556b5e9f51500
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-16 04:12:20 +0400

    Add files via upload

--- public/wheel.js
+++ public/wheel.js
@@ -6,6 +6,7 @@ const spinSound=document.getElementById("spinSound");
 const winSound=document.getElementById("winSound");
 
 const segments=44;
+
 const values=[
 ...Array(37).fill("0 AZN"),
 ...Array(4).fill("1 AZN"),
@@ -17,11 +18,11 @@ let angle=0;
 
 function draw(){
 
-const cx=300;
-const cy=300;
-const r=280;
+const cx=325;
+const cy=325;
+const r=300;
 
-ctx.clearRect(0,0,600,600);
+ctx.clearRect(0,0,650,650);
 
 for(let i=0;i<segments;i++){
 
@@ -32,17 +33,16 @@ ctx.beginPath();
 ctx.moveTo(cx,cy);
 ctx.arc(cx,cy,r,start,end);
 
-ctx.fillStyle=i%2?"#d4af37":"#222";
+ctx.fillStyle=i%2?"#FFD700":"#111";
 ctx.fill();
 
 ctx.save();
-
 ctx.translate(cx,cy);
 ctx.rotate(start+(Math.PI/segments));
 
-ctx.fillStyle="white";
-ctx.font="14px Arial";
-ctx.fillText(values[i],180,5);
+ctx.fillStyle="#fff";
+ctx.font="bold 20px Arial";
+ctx.fillText(values[i],200,5);
 
 ctx.restore();
 
@@ -54,7 +54,7 @@ draw();
 
 function spin(user="User"){
 
-document.getElementById("spinInfo").innerText="🎡 Çarx "+user+" üçün fırlanır";
+document.getElementById("spinInfo").innerText="🎡 "+user+" üçün çarx fırlanır";
 
 spinSound.play();
 
@@ -67,7 +67,7 @@ let t=(Date.now()-start)/duration;
 
 if(t<1){
 
-angle+=0.35*(1-t);
+angle+=0.4*(1-t);
 draw();
 requestAnimationFrame(frame);
 
@@ -88,16 +88,18 @@ function finish(){
 
 let result=values[Math.floor(Math.random()*values.length)];
 
+const el=document.getElementById("result");
+
 if(result==="0 AZN"){
-
-document.getElementById("result").innerText="😢 Uduzdunuz";
-
+el.innerText="😢 Uduzdunuz";
 }else{
-
 winSound.play();
-document.getElementById("result").innerText="🎉 Qazandınız: "+result;
-
+el.innerText="🎉 QAZANDINIZ "+result;
 }
+
+setTimeout(()=>{
+el.innerText="";
+},10000);
 
 countdown();
 
@@ -106,20 +108,16 @@ countdown();
 function countdown(){
 
 let t=10;
-
 let el=document.getElementById("countdown");
 
 let i=setInterval(()=>{
 
-el.innerText="Növbəti spin "+t;
-
+el.innerText="⏳ Növbəti spin "+t;
 t--;
 
 if(t<0){
-
 clearInterval(i);
 el.innerText="";
-
 }
 
 },1000);
@@ -131,39 +129,17 @@ window.spin=spin;
 const socket = io();
 
 socket.on("spinStart",(data)=>{
-  spin(data.user);
-});
-
-socket.on("spinResult",(data)=>{
-  console.log("Winner:",data.user,data.result);
-});
-
-socket.on("queueUpdate",(q)=>{
-  console.log("Queue:",q);
-});
-
-socket.on("tiktokStatus",(status)=>{
-
-const el=document.getElementById("tiktokStatus");
-
-if(status==="connected"){
-el.innerText="🟢 TikTok LIVE qoşuldu";
-}
-
-if(status==="failed"){
-el.innerText="🔴 TikTok qoşulmadı";
-}
-
+spin(data.user);
 });
 
 socket.on("topLike",(list)=>{
 
 let html="";
 list.forEach((u,i)=>{
- html += (i+1)+". "+u[0]+" "+u[1]+"<br>";
+html+=(i+1)+". "+u[0]+" "+u[1]+"<br>";
 });
 
-document.getElementById("topLike").innerHTML = html || "No data";
+document.getElementById("topLike").innerHTML=html||"No data";
 
 });
 
@@ -171,9 +147,20 @@ socket.on("topGift",(list)=>{
 
 let html="";
 list.forEach((u,i)=>{
- html += (i+1)+". "+u[0]+" "+u[1]+"<br>";
+html+=(i+1)+". "+u[0]+" "+u[1]+"<br>";
 });
 
-document.getElementById("topGift").innerHTML = html || "No data";
+document.getElementById("topGift").innerHTML=html||"No data";
+
+});
+
+socket.on("topWinners",(list)=>{
+
+let html="";
+list.forEach((u,i)=>{
+html+=(i+1)+". "+u.user+" "+u.prize+" AZN<br>";
+});
+
+document.getElementById("topWinners").innerHTML=html||"No data";
 
 });
