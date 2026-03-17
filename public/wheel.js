19e352baa151f3dba0dd8cc74da2439a59c310d3
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-17 23:33:44 +0400

    Add files via upload

--- public/wheel.js
+++ public/wheel.js
@@ -281,8 +281,7 @@ socket.on("topLike",(list)=>{
 
 let html="";
 list.forEach((u,i)=>{
- let cls = i===0 ? 'first' : '';
- html+=`<div class="${cls}">${i+1}. ${u[0]} ${u[1]}</div>`;
+ html+=(i+1)+". "+u[0]+" "+u[1]+"<br>";
 });
 
 document.getElementById("topLike").innerHTML=html||"No data";
@@ -293,8 +292,7 @@ socket.on("topGift",(list)=>{
 
 let html="";
 list.forEach((u,i)=>{
- let cls = i===0 ? 'first' : '';
- html+=`<div class="${cls}">${i+1}. ${u[0]} ${u[1]}</div>`;
+ html+=(i+1)+". "+u[0]+" "+u[1]+"<br>";
 });
 
 document.getElementById("topGift").innerHTML=html||"No data";
