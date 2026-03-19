33dedb70f6cb092357323685dd1cdbaa319f7d6b
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-20 01:10:45 +0400

    Add files via upload

--- public/race.js
+++ public/race.js
@@ -6,33 +6,36 @@ c.width = window.innerWidth;
 c.height = window.innerHeight;
 
 let players = [];
-let raceStarted = false;
+let images = {};
+let carImg = new Image();
+carImg.src = "https://i.imgur.com/7QFQZ5K.png"; // sample car
 
-socket.emit("join",{username:"user"+Math.random().toFixed(3)});
-
-socket.on("countdown",(n)=>{
-    document.getElementById("count").innerText = "START IN "+n;
-});
-
-socket.on("winner",(name)=>{
-    document.getElementById("count").innerText = "🏆 "+name+" QAZANDI!";
-});
+setInterval(()=>{
+    document.getElementById("popup").style.display="block";
+    setTimeout(()=>document.getElementById("popup").style.display="none",2000);
+},4000);
 
 socket.on("update",(data)=>{
-    players = data.players;
-    raceStarted = data.raceStarted;
+    players = data.players || [];
     draw();
 });
+
+function loadAvatar(url,u){
+    if(images[u]) return;
+    let img = new Image();
+    img.src = url;
+    images[u]=img;
+}
 
 function draw(){
     ctx.fillStyle="#0a0";
     ctx.fillRect(0,0,c.width,c.height);
 
     // road
-    ctx.fillStyle="#333";
+    ctx.fillStyle="#444";
     ctx.fillRect(0,200,c.width,200);
 
-    // lines
+    // road lines
     ctx.strokeStyle="white";
     for(let i=0;i<c.width;i+=40){
         ctx.beginPath();
@@ -41,13 +44,41 @@ function draw(){
         ctx.stroke();
     }
 
+    // spectators (simple animation)
+    for(let i=0;i<20;i++){
+        ctx.fillStyle="yellow";
+        ctx.fillRect(i*80,150 + Math.sin(Date.now()/200+i)*5,10,20);
+    }
+
     players.forEach((p,i)=>{
-        let y = 220 + i*40;
+        let y = 220 + i*50;
 
-        ctx.fillStyle="red";
-        ctx.fillRect(p.position,y,40,20);
+        loadAvatar(p.avatar,p.username);
 
+        // car sprite
+        ctx.drawImage(carImg,p.position,y,80,40);
+
+        // avatar inside car
+        if(images[p.username]){
+            ctx.drawImage(images[p.username],p.position+25,y+5,30,30);
+        }
+
+        // boost flame
+        if(p.speed>5){
+            ctx.fillStyle="orange";
+            ctx.beginPath();
+            ctx.arc(p.position-10,y+20,10,0,Math.PI*2);
+            ctx.fill();
+        }
+
+        // username
         ctx.fillStyle="white";
         ctx.fillText(p.username,p.position,y-5);
     });
+
+    // start light effect
+    ctx.fillStyle="red";
+    ctx.beginPath();
+    ctx.arc(c.width/2,100,20,0,Math.PI*2);
+    ctx.fill();
 }
