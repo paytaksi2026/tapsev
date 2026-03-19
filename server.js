e8653b339202eafec42a942aac601297a5aaea83
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-19 18:36:02 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -10,33 +10,44 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-let players = [];
-let raceActive = false;
+let queue = [];
+let racePlayers = [];
+let totalCoins = 0;
+let leaderboard = {};
 
-io.on("connection", (socket) => {
-  console.log("User connected");
+function tryStartRace(){
+  if(queue.length >= 5){
+    racePlayers = queue.splice(0,5);
+    io.emit("racePlayers", racePlayers);
+    io.emit("raceStart");
+  }
+}
 
-  socket.on("join", (user) => {
-    if (players.length < 5 && !raceActive) {
-      players.push(user);
-      io.emit("players", players);
+io.on("connection", (socket)=>{
+  console.log("Connected");
+
+  // simulate TikTok gift
+  socket.on("gift", (user)=>{
+    totalCoins += 10;
+
+    if(!queue.includes(user)){
+      queue.push(user);
     }
+
+    tryStartRace();
+    io.emit("queue", queue);
   });
 
-  socket.on("startRace", () => {
-    if (players.length === 5) {
-      raceActive = true;
-      io.emit("raceStart");
-    }
-  });
+  socket.on("finish", (winner)=>{
+    let reward = totalCoins * 0.1;
 
-  socket.on("finish", (winner) => {
-    io.emit("winner", winner);
-    players = [];
-    raceActive = false;
+    leaderboard[winner] = (leaderboard[winner] || 0) + 1;
+
+    io.emit("winner", {winner, reward, leaderboard});
+
+    totalCoins = 0;
+    racePlayers = [];
   });
 });
 
-server.listen(PORT, () => {
-  console.log("Server running on " + PORT);
-});
+server.listen(PORT, ()=>console.log("Running " + PORT));
