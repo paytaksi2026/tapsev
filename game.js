55803c5ef0da325395d751a8e4e92c62c35663e0
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-19 19:23:12 +0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,1 +1,61 @@
-// game logic placeholder
+const socket = io();
+let cars={};
+
+socket.on("racePlayers",(players)=>{
+  const track=document.getElementById("track");
+  track.innerHTML="";
+  cars={};
+
+  players.forEach(p=>{
+    let div=document.createElement("div");
+    div.className="car";
+
+    let img=document.createElement("img");
+    img.src=p.avatar;
+
+    let name=document.createElement("span");
+    name.innerText=p.user;
+
+    let bar=document.createElement("div");
+    bar.className="bar";
+    bar.style.width="50px";
+
+    div.appendChild(img);
+    div.appendChild(name);
+    div.appendChild(bar);
+
+    track.appendChild(div);
+    cars[p.user]=bar;
+  });
+
+  document.getElementById("engine").play();
+});
+
+socket.on("raceStart",()=>{
+  let interval=setInterval(()=>{
+    for(let u in cars){
+      let w=parseInt(cars[u].style.width);
+      cars[u].style.width=(w+Math.random()*40)+"px";
+      if(w>900){
+        socket.emit("finish",{user:u});
+        document.getElementById("finishSound").play();
+        clearInterval(interval);
+      }
+    }
+  },300);
+});
+
+socket.on("winner",(data)=>{
+  document.getElementById("winner").innerText=
+  "🏆 "+data.winner.user+" qalib oldu!";
+
+  let html="<h3>Ən yaxşı 15</h3>";
+  let sorted=Object.entries(data.leaderboard)
+  .sort((a,b)=>b[1]-a[1]).slice(0,15);
+
+  sorted.forEach((u,i)=>{
+    html+=(i+1)+". "+u[0]+" ("+u[1]+")<br>";
+  });
+
+  document.getElementById("leaderboard").innerHTML=html;
+});
--- server.js
+++ server.js
@@ -8,22 +8,17 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
-
 app.use(express.static(__dirname));
 
 let queue = [];
-let racePlayers = [];
 let totalCoins = 0;
 let leaderboard = {};
 
 const tiktok = new WebcastPushConnection(config.tiktokUser);
 
-tiktok.connect().then(() => {
-  console.log("TikTok LIVE connected");
-}).catch(err => console.error(err));
+tiktok.connect().then(()=>console.log("TikTok qoşuldu"));
 
-tiktok.on("gift", (data) => {
+tiktok.on("gift",(data)=>{
   let coins = data.diamondCount || 0;
   let user = data.uniqueId;
   let avatar = data.profilePictureUrl;
@@ -32,28 +27,24 @@ tiktok.on("gift", (data) => {
 
   totalCoins += coins;
 
-  if(!queue.find(u => u.user === user)){
+  if(!queue.find(u=>u.user===user)){
     queue.push({user, avatar});
   }
 
-  if(queue.length >= 5){
-    racePlayers = queue.splice(0,5);
-    io.emit("racePlayers", racePlayers);
+  if(queue.length>=5){
+    let players = queue.splice(0,5);
+    io.emit("racePlayers", players);
     io.emit("raceStart");
   }
 });
 
-io.on("connection", (socket)=>{
-  socket.on("finish", (winner)=>{
-    let reward = totalCoins * 0.1;
-
-    leaderboard[winner.user] = (leaderboard[winner.user] || 0) + 1;
-
-    io.emit("winner", {winner, reward, leaderboard});
-
-    totalCoins = 0;
-    racePlayers = [];
+io.on("connection",(socket)=>{
+  socket.on("finish",(winner)=>{
+    let reward = totalCoins*0.1;
+    leaderboard[winner.user]=(leaderboard[winner.user]||0)+1;
+    io.emit("winner",{winner,reward,leaderboard});
+    totalCoins=0;
   });
 });
 
-server.listen(PORT, ()=>console.log("Running " + PORT));
+server.listen(3000,()=>console.log("Server işləyir"));
