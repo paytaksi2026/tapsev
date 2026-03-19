d7830f9cb2d3d32ff0dce1a0d228378023aaf509
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-20 02:04:18 +0400

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,1 +1,31 @@
-// frontend placeholder
+
+const socket = io();
+
+let username = "user" + Math.floor(Math.random()*1000);
+
+socket.emit('join',{username});
+
+socket.on('players', (players)=>{
+    render(players);
+});
+
+socket.on('update', (players)=>{
+    render(players);
+});
+
+socket.on('winner', (w)=>{
+    document.getElementById('winner').innerText = "Qalib: " + w.username;
+});
+
+function render(players){
+    let track = document.getElementById('track');
+    track.innerHTML = '';
+
+    players.forEach(p=>{
+        let div = document.createElement('div');
+        div.className = 'car';
+        div.style.marginLeft = p.progress + 'px';
+        div.innerText = p.username;
+        track.appendChild(div);
+    });
+}
--- server.js
+++ server.js
@@ -1,2 +1,65 @@
-// Node.js server placeholder
-console.log('Server placeholder');
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+let players = [];
+let raceStarted = false;
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('join', (user) => {
+        if(players.length < 5 && !raceStarted){
+            players.push({...user, progress:0, speed:0});
+            io.emit('players', players);
+
+            if(players.length === 5){
+                startRace();
+            }
+        }
+    });
+
+    socket.on('like', (username) => {
+        let p = players.find(x=>x.username===username);
+        if(p) p.speed += 0.5;
+    });
+
+    socket.on('gift', (username) => {
+        let p = players.find(x=>x.username===username);
+        if(p) p.speed += 2;
+    });
+});
+
+function startRace(){
+    raceStarted = true;
+    io.emit('countdown');
+
+    setTimeout(()=>{
+        let interval = setInterval(()=>{
+            players.forEach(p=>{
+                p.progress += p.speed;
+            });
+
+            io.emit('update', players);
+
+            let winner = players.find(p=>p.progress >= 100);
+            if(winner){
+                clearInterval(interval);
+                io.emit('winner', winner);
+                players = [];
+                raceStarted = false;
+            }
+        },1000);
+    },10000);
+}
+
+server.listen(3000, ()=>{
+    console.log('Server running');
+});
