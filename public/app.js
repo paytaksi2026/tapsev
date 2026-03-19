45634eea5fe913c87e8849450246ce7d17ce4a30
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-20 02:41:40 +0400

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,29 +1,51 @@
 
 const socket = io();
 
-let lastPlayers = [];
+let players = [];
 
-socket.on('players', p => { lastPlayers = p; render(p); });
-socket.on('update', p => { lastPlayers = p; render(p); });
+socket.on('players', p => { players = p; render(); });
+socket.on('update', p => { players = p; render(); });
 
-function render(players){
+socket.on('countdown', ()=>{
+    startCountdown();
+});
+
+function startCountdown(){
+    let c = 10;
+    const el = document.getElementById('countdown');
+    el.innerText = "START " + c;
+    let i = setInterval(()=>{
+        c--;
+        el.innerText = "START " + c;
+        if(c<=0){
+            clearInterval(i);
+            el.innerText = "";
+        }
+    },1000);
+}
+
+function render(){
     let track = document.getElementById('track');
     track.innerHTML='';
 
-    // ALWAYS show 5 lanes even if empty
     for(let i=0;i<5;i++){
         let lane = document.createElement('div');
         lane.className='lane';
 
-        let p = players[i];
+        // spectators
+        let crowd = document.createElement('div');
+        crowd.className='crowd';
+        crowd.innerText = "🧍 🧍‍♀️ 🧍‍♂️ 🧍 🧍‍♂️";
+        lane.appendChild(crowd);
 
+        let p = players[i];
         if(p){
             let car = document.createElement('div');
             car.className='car';
             car.style.left = p.progress+'px';
 
             car.innerHTML = `
-                <div class="avatar"></div>
+                🔥🚗
                 <div class="name">${p.username}</div>
             `;
 
