e4e6f4077c7a1ef91eecef6d296be734dee4f683
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-20 02:29:50 +0400

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,64 +1,35 @@
 
 const socket = io();
 
-socket.on('players', render);
-socket.on('update', render);
+let lastPlayers = [];
 
-socket.on('countdown', ()=>{
-    playSound('start.mp3');
-});
-
-socket.on('winner', (w)=>{
-    playSound('win.mp3');
-    document.getElementById('winner').innerHTML =
-        "🏆 <b>" + w.user + "</b> qazandı (" + w.reward + ")";
-    loadTop();
-});
-
-socket.on('like_stream', (u)=>{
-    let el = document.getElementById('likes');
-    let span = document.createElement('span');
-    span.className = "likeItem";
-    span.innerText = u;
-    el.appendChild(span);
-    setTimeout(()=>span.remove(),4000);
-});
+socket.on('players', p => { lastPlayers = p; render(p); });
+socket.on('update', p => { lastPlayers = p; render(p); });
 
 function render(players){
     let track = document.getElementById('track');
     track.innerHTML='';
 
-    players.forEach((p,i)=>{
+    // ALWAYS show 5 lanes even if empty
+    for(let i=0;i<5;i++){
         let lane = document.createElement('div');
         lane.className='lane';
 
-        let car = document.createElement('div');
-        car.className='car';
-        car.style.left = p.progress+'px';
+        let p = players[i];
 
-        car.innerHTML = `
-            <div class="avatar"></div>
-            <div class="name">${p.username}</div>
-        `;
+        if(p){
+            let car = document.createElement('div');
+            car.className='car';
+            car.style.left = p.progress+'px';
 
-        lane.appendChild(car);
+            car.innerHTML = `
+                <div class="avatar"></div>
+                <div class="name">${p.username}</div>
+            `;
+
+            lane.appendChild(car);
+        }
+
         track.appendChild(lane);
-    });
-}
-
-function loadTop(){
-    fetch('/top')
-    .then(r=>r.json())
-    .then(data=>{
-        let lb = document.getElementById('leaderboard');
-        lb.innerHTML = "<h3>🏆 TOP 10</h3>";
-        data.forEach(d=>{
-            lb.innerHTML += d.username + " ("+d.wins+")<br>";
-        });
-    });
-}
-
-function playSound(file){
-    let audio = new Audio(file);
-    audio.play();
+    }
 }
