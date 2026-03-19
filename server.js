c5036f9e0467c60a6fdbe0ca8d09cb680cb6bab3
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-19 18:33:10 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-app.get("/api/status", (req, res) => {
-  res.json({ status: "OK", message: "TikTok Race Server Running" });
+let players = [];
+let raceActive = false;
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("join", (user) => {
+    if (players.length < 5 && !raceActive) {
+      players.push(user);
+      io.emit("players", players);
+    }
+  });
+
+  socket.on("startRace", () => {
+    if (players.length === 5) {
+      raceActive = true;
+      io.emit("raceStart");
+    }
+  });
+
+  socket.on("finish", (winner) => {
+    io.emit("winner", winner);
+    players = [];
+    raceActive = false;
+  });
 });
 
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+server.listen(PORT, () => {
+  console.log("Server running on " + PORT);
 });
