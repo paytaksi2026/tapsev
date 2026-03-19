fb42e9ce8b146081200a44adaa2eb7cf3adb3b0e
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-19 17:18:29 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -12,21 +13,34 @@ app.use(express.static("public"));
 let players = {};
 
 const tiktok = new WebcastPushConnection("xeberx.az");
-
 tiktok.connect().then(()=>console.log("TikTok connected"));
 
 tiktok.on("gift", data=>{
   if(data.diamondCount >= 10){
-    players[data.uniqueId] = players[data.uniqueId] || {hp:100,size:20};
-    players[data.uniqueId].hp += 20;
+    if(!players[data.uniqueId]){
+      players[data.uniqueId] = {
+        x: Math.random()*800,
+        y: Math.random()*600,
+        size: 20,
+        hp:100
+      };
+    }
     players[data.uniqueId].size += 5;
+    players[data.uniqueId].hp += 20;
     io.emit("update", players);
   }
 });
 
 tiktok.on("like", data=>{
   if(data.totalLikeCount >= 1000){
-    players[data.uniqueId] = players[data.uniqueId] || {hp:100,size:20};
+    if(!players[data.uniqueId]){
+      players[data.uniqueId] = {
+        x: Math.random()*800,
+        y: Math.random()*600,
+        size: 20,
+        hp:100
+      };
+    }
     io.emit("update", players);
   }
 });
@@ -34,12 +48,32 @@ tiktok.on("like", data=>{
 setInterval(()=>{
   const ids = Object.keys(players);
   ids.forEach(id=>{
-    players[id].hp -= 1;
-    if(players[id].hp <=0) delete players[id];
+    let p = players[id];
+    p.x += (Math.random()-0.5)*10;
+    p.y += (Math.random()-0.5)*10;
+    p.hp -= 0.5;
+    if(p.hp <=0) delete players[id];
   });
+
+  // collision
+  ids.forEach(a=>{
+    ids.forEach(b=>{
+      if(a!==b && players[a] && players[b]){
+        let dx = players[a].x - players[b].x;
+        let dy = players[a].y - players[b].y;
+        let dist = Math.sqrt(dx*dx+dy*dy);
+        if(dist < players[a].size){
+          if(players[a].size > players[b].size){
+            players[b].hp -= 2;
+          } else {
+            players[a].hp -= 2;
+          }
+        }
+      }
+    });
+  });
+
   io.emit("update", players);
-}, 1000);
+}, 50);
 
-io.on("connection", socket=>{});
-
-server.listen(3000, ()=>console.log("Running"));
+server.listen(3000);
