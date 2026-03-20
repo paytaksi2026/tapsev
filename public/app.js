bf0c428b0cd0e07474a9304634630640136f6824
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-20 09:34:08 +0400

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,10 +1,9 @@
 
 const socket = io();
-
 let size=1;
 
-socket.on('update', (data)=>{
-  let users = data.users;
+socket.on('update',(data)=>{
+  let users=data.users;
 
   let likeList = Object.entries(users)
     .sort((a,b)=>b[1].likes-a[1].likes)
@@ -20,10 +19,33 @@ socket.on('update', (data)=>{
   document.getElementById('gifts').innerHTML =
     giftList.map(u=>"<li>"+u[0]+" "+u[1].gifts+"</li>").join("");
 
-  grow(0.05);
+  document.getElementById('countdown').innerText = "Time: "+data.timer;
+
+  grow(0.03);
+});
+
+socket.on('winner',(data)=>{
+  alert("QALİB: "+data.user+" qazandı "+data.reward);
+  fireworks();
 });
 
 function grow(v){
   size+=v;
   document.getElementById('balloon').style.transform='scale('+size+')';
+}
+
+function fireworks(){
+  const canvas=document.getElementById('fireworks');
+  const ctx=canvas.getContext('2d');
+  canvas.width=window.innerWidth;
+  canvas.height=window.innerHeight;
+
+  for(let i=0;i<100;i++){
+    ctx.fillStyle="hsl("+Math.random()*360+",100%,50%)";
+    ctx.beginPath();
+    ctx.arc(Math.random()*canvas.width, Math.random()*canvas.height, 3,0,Math.PI*2);
+    ctx.fill();
+  }
+
+  setTimeout(()=>ctx.clearRect(0,0,canvas.width,canvas.height),2000);
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const { Pool } = require('pg');
 const path = require('path');
 
 const app = express();
@@ -9,29 +10,61 @@ const serverHttp = http.createServer(app);
 const io = new Server(serverHttp);
 
 const PORT = process.env.PORT || 3000;
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 let users = {};
 let totalGifts = 0;
+let timer = 300;
+
+async function saveWinner(username, reward){
+  await pool.query(
+    "INSERT INTO winners(username, reward) VALUES($1,$2)",
+    [username, reward]
+  );
+}
 
 io.on('connection', (socket) => {
-  console.log('User connected');
+  console.log("Client connected");
 
-  // DEMO (later TikTok will trigger this)
-  setInterval(()=>{
-    let user = "user"+Math.floor(Math.random()*5);
-
-    if(!users[user]) users[user]={likes:0,gifts:0};
-
-    users[user].likes += 1;
-    users[user].gifts += Math.floor(Math.random()*3);
-
-    totalGifts += users[user].gifts;
-
-    io.emit('update', {users, totalGifts});
-  },2000);
-
+  socket.emit("status", "ONLINE");
 });
+
+setInterval(()=>{
+  timer--;
+
+  if(timer <= 0){
+    let winner = Object.entries(users)
+      .sort((a,b)=>b[1].gifts-a[1].gifts)[0];
+
+    if(winner){
+      let reward = totalGifts * 0.1;
+      saveWinner(winner[0], reward);
+
+      io.emit("winner", {user:winner[0], reward});
+    }
+
+    users = {};
+    totalGifts = 0;
+    timer = 300;
+  }
+
+  io.emit("update", {users, totalGifts, timer});
+
+},1000);
+
+// DEMO growth (replace with TikTok events later)
+setInterval(()=>{
+  let user = "user"+Math.floor(Math.random()*10);
+
+  if(!users[user]) users[user]={likes:0,gifts:0};
+
+  users[user].likes += 1;
+  users[user].gifts += Math.floor(Math.random()*5);
+
+  totalGifts += 5;
+
+},2000);
 
 serverHttp.listen(PORT, ()=>console.log("Running "+PORT));
