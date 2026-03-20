2ed9db9f66e02633c23340a838a84e515b9f7949
paytaksi2026 <paytaksi2026@gmail.com>
2026-03-20 18:26:32 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -14,10 +14,12 @@ const USERNAME=process.env.TIKTOK_USERNAME||"xeberx.az";
 
 let users={};
 let timer=180;
-let phase="game"; // game | pause
+let phase="game";
 let pauseTime=15;
-
 let winners=[];
+
+let totalLikes=0;
+let totalGifts=0;
 
 const tiktok=new WebcastPushConnection(USERNAME);
 tiktok.connect().then(()=>console.log("TikTok Connected"));
@@ -27,6 +29,7 @@ tiktok.on("like",data=>{
  const u=data.uniqueId;
  if(!users[u]) users[u]={likes:0,gifts:0};
  users[u].likes+=data.likeCount||1;
+ totalLikes += data.likeCount||1;
 });
 
 tiktok.on("gift",data=>{
@@ -34,22 +37,32 @@ tiktok.on("gift",data=>{
  const u=data.uniqueId;
  if(!users[u]) users[u]={likes:0,gifts:0};
  users[u].gifts+=data.diamondCount||1;
+ totalGifts += data.diamondCount||1;
 });
+
+function explodeNow(){
+ let w=Object.entries(users).sort((a,b)=>b[1].gifts-a[1].gifts)[0];
+ if(w){
+   winners.unshift(w[0]);
+   if(winners.length>10) winners.pop();
+   io.emit("winner",{user:w[0],reward:0});
+ }
+ phase="pause";
+ pauseTime=15;
+}
 
 setInterval(()=>{
 
  if(phase==="game"){
    timer--;
 
+   // SMART EXPLOSION
+   if(totalLikes>=10000 || totalGifts>=5000){
+     explodeNow();
+   }
+
    if(timer<=0){
-     let w=Object.entries(users).sort((a,b)=>b[1].gifts-a[1].gifts)[0];
-     if(w){
-       winners.unshift(w[0]);
-       if(winners.length>10) winners.pop();
-       io.emit("winner",{user:w[0],reward:0});
-     }
-     phase="pause";
-     pauseTime=15;
+     explodeNow();
    }
 
  } else if(phase==="pause"){
@@ -57,6 +70,8 @@ setInterval(()=>{
 
    if(pauseTime<=0){
      users={};
+     totalLikes=0;
+     totalGifts=0;
      timer=180;
      phase="game";
    }
