bb61f58b612f4615306af8a533a03ee5335a3c8d
paytaksi2026 <paytaksi2026@gmail.com>
2026-04-01 01:52:07 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 let protocol = location.protocol === "https:" ? "wss://" : "ws://";
 let ws = new WebSocket(protocol + location.host);
 
@@ -6,13 +5,13 @@ let queue=[];
 let winners={};
 let likes={};
 let gifts={};
-let rewards={};
 
 ws.onmessage = (e)=>{
  let data = JSON.parse(e.data);
 
  if(data.type==='like'){
    likes[data.user]=(likes[data.user]||0)+data.count;
+
    if(likes[data.user]>=1000){
      queue.push(data.user);
      likes[data.user]=0;
@@ -21,13 +20,14 @@ ws.onmessage = (e)=>{
 
  if(data.type==='gift'){
    gifts[data.user]=(gifts[data.user]||0)+data.diamonds;
+
    if(gifts[data.user]>=100){
      queue.push(data.user);
      gifts[data.user]=0;
    }
  }
 
- updateLists();
+ update();
 };
 
 function avatar(u){
@@ -37,14 +37,17 @@ function avatar(u){
 function render(id,data){
  let el=document.getElementById(id);
  el.innerHTML="";
- Object.entries(data).sort((a,b)=>b[1]-a[1]).slice(0,15).forEach(([u,v])=>{
+ Object.entries(data)
+ .sort((a,b)=>b[1]-a[1])
+ .slice(0,15)
+ .forEach(([u,v])=>{
   let li=document.createElement("li");
   li.innerHTML="<img src='"+avatar(u)+"' width=25> "+u+" ("+v+")";
   el.appendChild(li);
  });
 }
 
-function updateLists(){
+function update(){
  render("topLikes",likes);
  render("topGifts",gifts);
  render("winners",winners);
@@ -57,20 +60,20 @@ function roll(user){
  document.getElementById("dice1").innerText=d1;
  document.getElementById("dice2").innerText=d2;
 
- new Audio("sounds/dice.mp3").play();
-
  if(d1==6 && d2==6){
    winners[user]=(winners[user]||0)+1;
-   rewards[user]=(rewards[user]||0)+1;
 
-   document.getElementById("result").innerText="ŞEŞ QOŞA!";
-   new Audio("sounds/win.mp3").play();
+   document.getElementById("result").innerText="🔥 ŞEŞ QOŞA 🔥";
+
+   document.body.style.background="radial-gradient(circle, red, black)";
+   setTimeout(()=>{document.body.style.background=""},2000);
+
  } else {
    document.getElementById("result").innerText="Uduzdu";
  }
 }
 
-function processQueue(){
+function process(){
  if(queue.length==0)return;
 
  let user=queue.shift();
@@ -79,16 +82,16 @@ function processQueue(){
 
  popup.innerText=user+" hazır ol "+c;
 
- let int=setInterval(()=>{
+ let i=setInterval(()=>{
   c--;
   popup.innerText=user+" hazır ol "+c;
 
   if(c<=0){
-    clearInterval(int);
+    clearInterval(i);
     popup.innerText="";
     roll(user);
   }
  },1000);
 }
 
-setInterval(processQueue,20000);
+setInterval(process,15000);
